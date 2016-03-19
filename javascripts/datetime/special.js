d9c34a0cd7ee069cbf51e4cf596448d9ca4efc88
Divya Mohan <divyamohan1993@users.noreply.github.com>
2016-03-19 13:42:23 +0530

    Update special.js

--- javascripts/datetime/special.js
+++ javascripts/datetime/special.js
@@ -12,7 +12,7 @@ var ehstart = 20 + ':' + 29;
 var ehstop = 21 + ':' + 31;
 var datenow = 19;
 var monthnow = 2; //March (Jan = 0)
-var earthhourredirect = "/development/special/earth_hour/"
+var earthhourredirect = "/development/special/earth_hour/";
 
 if (d == datenow & mn == monthnow) {
 	if (timenow > ehstart & timenow < ehstop) {
