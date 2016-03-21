6a6f6d16ad8b6563df545e255b5e04e859118a34
Divya Mohan <divyamohan1993@gmail.com>
2016-03-21 15:58:57 +0530

    .

--- javascripts/datetime/special.js
+++ javascripts/datetime/special.js
@@ -7,15 +7,15 @@ var mn = date.getMonth();
 var yr = date.getYear();
 
 //  -----------Earth hour------------
-var timenow = h + ':' + m;
+var ehtimenow = h + ':' + m;
 var ehstart = 20 + ':' + 29;
 var ehstop = 21 + ':' + 31;
-var datenow = 19;
-var monthnow = 2; //March (Jan = 0)
+var ehdatenow = 19;
+var ehmonthnow = 2; //March (Jan = 0)
 var earthhourredirect = "/development/special/earth_hour/";
 
-if (d == datenow & mn == monthnow) {
-	if (timenow > ehstart & timenow < ehstop) {
+if (d == ehdatenow & mn == ehmonthnow) {
+	if (ehtimenow > ehstart & ehtimenow < ehstop) {
 //		All conditions true
 		top.document.getElementById('I1').setAttribute("src", earthhourredirect);	
 	}
@@ -26,22 +26,18 @@ else {
 }
 
 // --------------- Earth Day ---------------
-	var timenow = h + ':' + m;
-var edstart = 20 + ':' + 29;
-var edstop = 21 + ':' + 31;
 var eddatenow = 22;
 var edmonthnow = 2; //March (Jan = 0)
-var eartdayredirect = "/development/special/earth_hour/";
-var EarthDay = localStorage.getItem('EarthDay') || '';
- 
+var earthdayredirect = "/development/special/earth_day/";
+var EarthDay = sessionStorage.getItem('EarthDay') || '';
 
 if (d == eddatenow & mn == edmonthnow) {
 	if (EarthDay != 'yes') {
-	top.document.getElementById('I1').setAttribute("src", earthhourredirect);	
-//	alert("You");
-	localStorage.setItem('EarthDay','yes');
+	top.document.getElementById('I1').setAttribute("src", earthdayredirect);
+	sessionStorage.setItem('EarthDay','yes');
 	}
 	else {
 	}
+}
 else {
 }
