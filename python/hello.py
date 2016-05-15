eca9e34b265ab9743ac0ef1403e95e0245214845
Divya Mohan <divyamohan1993@gmail.com>
2016-05-15 08:11:12 +0530

    python

---
+++ python/hello.py
@@ -0,0 +1,11 @@
+import math
+import sys
+import autopy
+
+print('Hello World \n')
+# print(dir(math))
+# print(dir(sys))
+# print (sys.path)
+print (dir(autopy))
+
+
---
+++ python/hello_gui.py
@@ -0,0 +1,4 @@
+import autopy3
+def hello_there_world():
+    autopy3.alert.alert("Hello, world")
+hello_there_world()
