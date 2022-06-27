810b23508d6eae5763ca8aa2bead163684da36b4
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-06-27 10:17:45 +0530

    var.js common.js

--- javascripts/common.js
+++ javascripts/common.js
@@ -2,7 +2,10 @@ function copyright() {
     document.write('&copy; 2007-'+new Date().getFullYear()+' Divya Mohan');
 }
 
-function decodeencryptedvar(s) {
+// Decode Encrypted Variable's - Call this function to decode variables.
+// Example: execute the vars by calling <script>decodeencryptedvar(_varname_);</script> in html where it is required.
+// abbr: DECodeENcryptedVar - decenv
+function decenv(s) {
     var s1 = unescape(s.substr(0, s.length - 1));
     var t = '';
     for (i = 0; i < s1.length; i++) t += String.fromCharCode(s1.charCodeAt(i) - s.substr(s.length - 1, 1));
