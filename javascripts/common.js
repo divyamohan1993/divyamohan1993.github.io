ed8fa3aa11ef4e1d352ae5feb870c1dc90037c74
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-07-24 13:48:55 +0530

    .

--- javascripts/common.js
+++ javascripts/common.js
@@ -1,12 +1,12 @@
 function copyright() {
-    document.write('&copy; 2007-'+new Date().getFullYear()+' Divya Mohan');
+    document.write('&copy; 2007-' + new Date().getFullYear() + ' Divya Mohan & Vanshika');
 }
 
 // Decode Encrypted Variable's - Call this function to decode variables.
 // Example: execute the vars by calling <script>decenv(_varname_);</script> in html where it is required.
 // abbr: DeCodeEncryptedVAr - dceva
 function dcevar(s) {
-    var s1 =  decodeURIComponent(s.substr(0, s.length - 1));
+    var s1 = decodeURIComponent(s.substr(0, s.length - 1));
     var t = '';
     for (i = 0; i < s1.length; i++) t += String.fromCharCode(s1.charCodeAt(i) - s.substr(s.length - 1, 1));
     var decoded = decodeURIComponent(t);
