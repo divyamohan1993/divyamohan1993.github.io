7191b228bf2057c716a5911460cd32dc8614b681
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-30 18:55:48 +0530

    .

--- edu/su/course/csu953/var.js
+++ edu/su/course/csu953/var.js
@@ -7,7 +7,7 @@ function get_menu_list() {
 
     if (path) {
         switch (path) {
-            case "class":
+            case "lab":
                 get_classes();
                 break;
             case "books":
@@ -29,7 +29,7 @@ function get_menu_list() {
         for (let i = 0; i < cases.length; i++) {
             switch (cases[i]) {
                 case 1:
-                    link = "class/";
+                    link = "lab/";
                     date = "Last Updated: Wed November 23, 2022";
                     title = "HTML Laboratory";
                     desc = "Lets learn to code! This section contains list of the labs from basic to advanced level which were done in class and is written in HTML.";
@@ -64,67 +64,43 @@ function get_menu_list() {
 
         for (let i = 0; i < cases.length; i++) {
 
-            var codetype = "C Language";
+            var codetype = "HTML";
             var d = i + 1;
-            var link = "p" + d;
-            var title = "Program " + d;
+            var link = "c" + d;
+            var title = "Lab " + d;
             var readtime = Math.floor(Math.random() * 5) + 2;
             var author = 1;
 
             switch (cases[i]) {
                 case 1:
-                    link = "c1";
                     date = "Thu September 27, 2022";
-                    title = "Lab 1";
-                    desc = "This is an introductory class";
-                    codetype = "HTML";
-                    readtime = "2";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    desc = "This is an introductory class which discusses various aspects of web programming";
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 2:
-                    link = "c2";
                     date = "Thu September 27, 2022";
-                    title = "Lab 2";
                     desc = "Use of tags title, details, p, heading, and inline style of background-color.";
-                    codetype = "HTML";
-                    readtime = "2";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 3:
-                    link = "c3";
                     date = "Thu September 27, 2022";
-                    title = "Lab 3";
                     desc = "Use of tags tags sub, sup, text-align, address, and abbr.";
-                    codetype = "HTML";
-                    readtime = "2";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 4:
-                    link = "c4";
                     date = "Thu September 27, 2022";
-                    title = "Lab 4";
                     desc = "Class Test";
-                    codetype = "HTML";
-                    readtime = "2";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 5:
-                    link = "c5";
                     date = "Thu September 27, 2022";
-                    title = "Lab 5";
                     desc = "Use of table, tr, th tags in creation of Tables";
-                    codetype = "HTML";
-                    readtime = "4";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 6:
-                    link = "c6";
                     date = "Thu November 17, 2022";
-                    title = "Lab 6";
                     desc = "VIVA based on questions directed towards odd and even students.";
-                    codetype = "HTML";
-                    readtime = "5";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 default:
                     link = "#";
@@ -133,7 +109,7 @@ function get_menu_list() {
                     desc = "Please define a hierchy for the classes of course " + course + ".";
                     codetype = "#CourseNotDefined";
                     readtime = "0";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
             }
         }
@@ -150,7 +126,7 @@ function get_menu_list() {
                     desc = "This is an introductory class";
                     codetype = "HTML";
                     readtime = "2";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 2:
                     link = "c2";
@@ -159,7 +135,7 @@ function get_menu_list() {
                     desc = "Use of tags title, details, p, heading, and inline style of background-color.";
                     codetype = "HTML";
                     readtime = "2";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 default:
                     link = "#";
@@ -168,7 +144,7 @@ function get_menu_list() {
                     desc = "Please define a hierchy for the classes of course " + course + ".";
                     codetype = "#CourseNotDefined";
                     readtime = "0";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
             }
         }
