9a2c7d255eb1dffb9c900cfc0ec72ec8d0ba1d27
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-12-03 20:19:14 +0530

    .

--- edu/su/course/fsu030/var.js
+++ edu/su/course/fsu030/var.js
@@ -12,11 +12,11 @@ function get_menu_list() {
             case "section":
                 get_section();
                 break;
-            case "books":
-                get_books();
+            case "seminar":
+                get_seminar();
                 break;
-            case "program":
-                get_programs();
+            case "assignment":
+                get_assignment();
                 break;
             default:
                 break;
@@ -27,25 +27,35 @@ function get_menu_list() {
     }
 
     function get_main() {
-        var cases = [1,2];
+        var cases = [1, 2, 3];
         for (let i = 0; i < cases.length; i++) {
             switch (cases[i]) {
                 case 1:
-                    link = "a1";
-                    date = "Last Updated: Wed November 02, 2022";
-                    title = "Assignment 1";
-                    desc = "Assignment 1 based on Mechanics.";
-                    codetype = "Mechanics";
-                    readtime = "10";
+                    link = "section/";
+                    date = "Last Updated: Sat December 03, 2022";
+                    title = "Sections";
+                    desc = "All topics included in different sections for the subject of physics.";
+                    codetype = "Syllabus";
+                    readtime = "";
+                    author = "";
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 2:
-                    link = "s2";
+                    link = "seminar/";
+                    date = "Last Updated: Sat December 03, 2022";
+                    title = "Seminars";
+                    desc = "Seminars conducted in the class";
+                    codetype = "Seminar";
+                    readtime = "";
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
+                    break;
+                case 3:
+                    link = "assignment/";
                     date = "Thu September 27, 2022";
-                    title = "Seminar 2";
-                    desc = "Seminar 2 based on EM Waves";
-                    codetype = "EM Waves";
-                    readtime = "15";
+                    title = "Assignments";
+                    desc = "Assignments which were given in the class";
+                    codetype = "Assignment";
+                    readtime = "";
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 default:
@@ -99,10 +109,10 @@ function get_menu_list() {
         var cases = [1, 2];
         for (let i = 0; i < cases.length; i++) {
 
-            var codetype = "Section B";
+            var codetype = "Physics";
             var d = i + 1;
             var link = "s" + d;
-            // var title = "Section " + d;
+            var title = "Section ";
             var readtime = Math.floor(Math.random() * 5) + 2;
             var author = 1;
 
@@ -110,19 +120,19 @@ function get_menu_list() {
                 case 1:
                     // link = "add_link like 'c2' ";
                     date = "Sat December 3, 2022";
-                    title = "Program 31 / title += 'append something' ";
-                    desc = "Hello World";
+                    title += "A: Work, Energy and Fluid motion";
+                    desc = "This topic discusses about Work, Energy and Fluid Motions namely Work and energy theorem, Conservative and non-conservative forces, Potential energy, Energy diagram, Stable and unstable equilibrium, Gravitational potential energy,  Elastic potential energy, Types of fluids, Significance of fluid mechanics, Fluid properties, Pascal's law, Surface tension and capillarity, Continuity equation, Bernoulli's equation.";
                     // codetype = "replace here with custom var";
                     // readtime = "2";
                     // author = "vp";
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 2:
-                    date = "Wed October 5, 2022";
-                    desc = "Program to calculate sum aftertaking two numbers as input from user.";
+                    date = "Sat December 3, 2022";
+                    title += "B: Electricity and Magnetism";
+                    desc = "This topic discusses about electricity and magnetism namely EM waves and dielectrics: Relationship between electric field & potential, Dielectric polarization, displacement current, Types of polarization, Maxwell's equations, Equation of EM waves in free space, Velocity of EM waves, Electromagnetic Spectrum (Basic idea) Magnetic materials & Superconductivity: Basic idea of Dia, Para, Ferro & Ferri, Ferrites, Magnetic anisotropy, Magnetostriction and its applications in production of Ultrasonic waves, Superconductivity, Superconductors as ideal diamagnetic materials, Signatures of Superconducting state, Type I & Type II superconductors.";
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
-                
                 case 36:
                     date = "Thu November 17, 2022";
                     desc = "VIVA based on questions directed towards odd and even students.";
@@ -140,34 +150,39 @@ function get_menu_list() {
             }
         }
     }
-
-    function get_books() {
-        var cases = [1, 2, 3, 4, 5, 6];
+    function get_seminar() {
+        var cases = [1, 2];
         for (let i = 0; i < cases.length; i++) {
+
+            var codetype = "seminar@physics";
+            var d = i + 1;
+            var link = "s" + d;
+            var title = "Seminar " + d;
+            var readtime = Math.floor(Math.random() * 5) + 2;
+            var author = 1;
+
             switch (cases[i]) {
                 case 1:
-                    link = "c1";
-                    date = "Thu September 27, 2022";
-                    title = "Lab 1";
-                    desc = "This is an introductory class";
-                    codetype = "BOOKS";
-                    readtime = "2";
+                    // link = "add_link like 'c2' ";
+                    date = "Sat December 3, 2022";
+                    title += ": Application of Fundamental Mechanics on a Rocket";
+                    desc = "This topic discusses about Work, Energy and Fluid Motions on a Saturn 5 Rocket.";
+                    // codetype = "replace here with custom var";
+                    // readtime = "2";
+                    // author = "vp";
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 2:
-                    link = "c2";
-                    date = "Thu September 27, 2022";
-                    title = "Lab 2";
-                    desc = "Use of tags title, details, p, heading, and inline style of background-color.";
-                    codetype = "BOOK2";
-                    readtime = "2";
+                    date = "Sat December 3, 2022";
+                    title += ": Electromagnetic Waves";
+                    desc = "This seminar was presented on Electromagnetic Waves";
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 default:
                     link = "#";
                     date = "Not Applicable";
-                    title = "Books for current " + course + " has not been defined.";
-                    desc = "Please define a hierchy for the classes of course " + course + ".";
+                    title = "Seminars for current course " + course + " has not been defined.";
+                    desc = "Please define a hierchy for the Seminars of course " + course + ".";
                     codetype = "#CourseNotDefined";
                     readtime = "0";
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
@@ -175,4 +190,46 @@ function get_menu_list() {
             }
         }
     }
+    function get_assignment() {
+        var cases = [1];
+        for (let i = 0; i < cases.length; i++) {
+
+            var codetype = "assignment@physics";
+            var d = i + 1;
+            var link = "a" + d;
+            var title = "Section ";
+            var readtime = Math.floor(Math.random() * 5) + 2;
+            var author = 1;
+
+            switch (cases[i]) {
+                case 1:
+                    // link = "add_link like 'c2' ";
+                    date = "Sat December 3, 2022";
+                    title += ": Application of Fundamental Mechanics on Saturn V Rocket.";
+                    desc = "This assignment is based on the fundamental mechanics of Saturn V Rocket";
+                    // codetype = "replace here with custom var";
+                    // readtime = "2";
+                    // author = "vp";
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
+                    break;
+                case 2:
+                    date = "Sat December 3, 2022";
+                    title += "B: Electricity and Magnetism";
+                    desc = "This topic discusses about electricity and magnetism namely EM waves and dielectrics: Relationship between electric field & potential, Dielectric polarization, displacement current, Types of polarization, Maxwell's equations, Equation of EM waves in free space, Velocity of EM waves, Electromagnetic Spectrum (Basic idea) Magnetic materials & Superconductivity: Basic idea of Dia, Para, Ferro & Ferri, Ferrites, Magnetic anisotropy, Magnetostriction and its applications in production of Ultrasonic waves, Superconductivity, Superconductors as ideal diamagnetic materials, Signatures of Superconducting state, Type I & Type II superconductors.";
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
+                    break;
+                default:
+                    link = "#";
+                    date = "Not Applicable";
+                    title = path + " for current course " + course + " has not been defined.";
+                    desc = "Please define a hierchy for the" + path + "of course " + course + ".";
+                    codetype = "#CourseNotDefined";
+                    readtime = "0";
+                    body_blockcards(link, date, title, desc, codetype, readtime, author);
+                    break;
+            }
+        }
+    }
+
+
 }
