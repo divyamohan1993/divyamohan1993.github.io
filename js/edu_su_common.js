93046b4bac99ce708825c9e70015e46ad16594c5
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-12-06 23:27:09 +0530

    .

--- js/edu_su_common.js
+++ js/edu_su_common.js
@@ -375,6 +375,21 @@ function copyright(rights) {
         if (localStorage.noshow == '1') {
             $('#cookie-notice').addClass('d-none');
         };
+
+        document.addEventListener("DOMContentLoaded", function () {
+            renderMathInElement(document.body, {
+                // customised options
+                // • auto-render specific keys, e.g.:
+                delimiters: [
+                    { left: '$$', right: '$$', display: true },
+                    { left: '$', right: '$', display: false },
+                    { left: '\\(', right: '\\)', display: false },
+                    { left: '\\[', right: '\\]', display: true }
+                ],
+                // • rendering keys, e.g.:
+                throwOnError: false
+            });
+        });
     };
 }
 
