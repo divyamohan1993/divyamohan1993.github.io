10626aa2875ff834af6bc2a631aa1b379535d43e
Divya Mohan <divyamohan1993@users.noreply.github.com>
2023-01-05 00:22:53 +0530

    .

--- edu/su/course/csu953/var.js
+++ edu/su/course/csu953/var.js
@@ -50,10 +50,10 @@ function get_menu_list() {
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
                     break;
                 case 3:
-                    link = "https://youtu.be/SKDKwalxgSQ";
+                    link = "project_report_video";
                     date = "Last Updated: Wed January 4, 2022";
                     title = "Project Report Video";
-                    desc = "Video of Project Report. Upon clicking, you will be redirected to YouTube.";
+                    desc = "Video of Project Report for CSU953. Shoolini University.";
                     codetype = "Video";
                     // readtime = "10";
                     body_blockcards(link, date, title, desc, codetype, readtime, author);
