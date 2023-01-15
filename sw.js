1207abf7fa6a46a41715da31b37aa86fdfb87308
Divya Mohan <divyamohan1993@users.noreply.github.com>
2023-01-15 19:36:49 +0530

    .

--- sw.js
+++ sw.js
@@ -76,120 +76,70 @@ self.addEventListener('fetch', (event) => {
 
 
 /******* CACHE ALL URLS OF DOMAIN AUTOMATICALLY *******/
-// Set a cache name
-const CACHE_NAME = 'shoolini-cache-v2';
 
-// Set the domain of the website
+const CACHE_NAME = 'shoolini-cache-v2';
 const DOMAIN = 'https://dmj.one';
 
-/* // On service worker installation
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    // Fetch all the pages of the website
-    fetch(DOMAIN)
-      .then((response) => response.text())
-      .then((data) => {
-        // Extract all the URLs from the fetched data
-        const urls = data.match(/(https?:\/\/[^\s]+)/g);
-        // Open a cache and add all the URLs to it
-        return caches.open(CACHE_NAME)
-          .then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(urls);
-          });
-      })
-  );
-}); */
-/* 
-// On service worker installation
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    // Fetch all the pages of the website
-    fetch(DOMAIN)
-      .then((response) => response.text())
-      .then((data) => {
-        // Extract all the URLs from the fetched data
-        const urls = data.match(/(https?:\/\/[^\s]+)/g);
-        const validUrls = urls.filter((url) => url.startsWith(DOMAIN));
-        // Open a cache and add all the URLs to it
-        return caches.open(CACHE_NAME)
-          .then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(validUrls);
-          });
-      })
-  );
-});
- */
+// Helper function to cache resources
+function cacheResources(urls) {
+  return caches.open(CACHE_NAME)
+    .then((cache) => {
+      return cache.addAll(urls);
+    });
+}
 
-// On service worker installation
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    // Fetch all the pages of the website
     fetch(DOMAIN)
-      .then((response) => response.text())
-      .then((data) => {
-        // Extract all the URLs from the fetched data
+      .then(response => response.text())
+      .then(data => {
+        // Extract URLs from the fetched data
         const urls = data.match(/(https?:\/\/[^\s]+)/g);
-        const validUrls = urls.filter((url) => url.startsWith(DOMAIN));
-        // Open a cache and add all the URLs to it
+        // Filter URLs that belong to the current domain
+        const validUrls = urls.filter(url => url.startsWith(DOMAIN));
+        // Open cache and add URLs
         return caches.open(CACHE_NAME)
-          .then((cache) => {
+          .then(cache => {
             console.log('Opened cache');
             return cache.addAll(validUrls);
           });
-      }).then(() => {
-        // Fetch resources and cache them
-        fetch('/sw_allurls.json')
-          .then((response) => response.json())
-          .then((data) => {
+      })
+      .then(() => {
+        // Fetch additional resources and cache them
+        return fetch('/sw_allurls.json')
+          .then(response => response.json())
+          .then(data => {
             // Extract URLs from the fetched data
             const urls = data.resources;
-            cacheResources(urls);
+            return cacheResources(urls);
           });
       })
   );
 });
 
-/* // On service worker activation
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
+        cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
       );
     })
-  );
-}); */
-
-// On service worker activation
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => {
-      // Fetch resources and cache them
-      fetch('/sw_allurls.json')
-        .then((response) => response.json())
-        .then((data) => {
-          // Extract URLs from the fetched data
-          const urls = data.resources;
-          cacheResources(urls);
-        });
-    })
+      .then(() => {
+        // Fetch additional resources and cache them
+        return fetch('/sw_allurls.json')
+          .then(response => response.json())
+          .then(data => {
+            // Extract URLs from the fetched data
+            const urls = data.resources;
+            return cacheResources(urls);
+          });
+      })
   );
 });
-
 
 // On service worker fetch
 self.addEventListener('fetch', (event) => {
