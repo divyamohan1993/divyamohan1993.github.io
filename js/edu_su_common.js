d858dc66d20671d77ca16cfa9985a7140485eba5
Divya Mohan <divyamohan1993@users.noreply.github.com>
2023-01-22 16:57:38 +0530

    .

--- js/edu_su_common.js
+++ js/edu_su_common.js
@@ -121,7 +121,7 @@
         }
     }
 
-    if (lastFolderIndex === 3 || lastFolderIndex === 4 || lastFolderIndex === 5 || lastFolderIndex === 6) {
+    if (lastFolderIndex === 4 || lastFolderIndex === 5 || lastFolderIndex === 6 || lastFolderIndex === 7) {
         varJsUrl = 'https://dmj.one/' + urlParts.slice(3, lastFolderIndex).join('/') + '/var.js';
     } // else {
     //     varJsUrl = 'https://dmj.one/' + urlParts.slice(3, lastFolderIndex + 1).join('/') + 'var.js';
