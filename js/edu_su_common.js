c06003c0a17d8431ccf3ca12c6cdd2c35b957e28
Divya Mohan <divyamohan1993@users.noreply.github.com>
2023-01-22 22:40:49 +0530

    .

--- js/edu_su_common.js
+++ js/edu_su_common.js
@@ -1,6 +1,11 @@
 /* 
  * This file contains all the js combined.
  */
+var notify_cookie = "%2B9Ijo%7C%2B86oj%2B9J%2B88iuuqok3tuzoik%2B88%2B86irgyy%2B9J%2B88%7D3766%2B86hm3jgxq%2B86zk~z3%7Dnozk%2B86vz39%2B86v~3%3A%2B86vh37%2B86vuyozout3lo~kj%2B88%2B86yz%7Frk%2B9J%2B88%C2%803otjk~%2B9G%2B867666%2B9H%2B86huzzus%2B9G%2B866%2B9H%2B88%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9Ijo%7C%2B86irgyy%2B9J%2B88iutzgotkx%2B86v38%2B88%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9Ijo%7C%2B86irgyy%2B9J%2B88xu%7D%2B88%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9Ijo%7C%2B86irgyy%2B9J%2B88iur3ys3%3E%2B86iur3sj3%3F%2B88%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9Iv%2B86irgyy%2B9J%2B88v38%2B88%2B9KZnoy%2B86%7Dkhyozk%2B86%7Byky%2B86iuuqoky%2B86yu%2B86zngz%2B86%7Dk%2B86igt%2B86vxu%7Cojk%2B86%7Fu%7B%2B86%7Dozn%2B86znk%2B86hkyz%2B86%7Dkhyozk%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86k~vkxoktik4%2B86H%7F%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86iroiqotm%2B86%2BK8%2B%3E6%2B%3FIO%2B86Giikvz%2BK8%2B%3E6%2B%3FJ%2B86ux%2B86h%7F%2B86yixurrotm%2B86zu%2B86%7Cok%7D%2B86znk%2B86iutzktzy%2B86ul%2B86znoy%2B86%7Dkhyozk%2B86%7Fu%7B%2B86giqtu%7Drkjmk%2B86znk%2B86%7Byk%2B86ul%2B86iuuqoky%2B86gtj%2B86zu%2B86u%7Bx%2B86%2B9Ig%2B86nxkl%2B9J%2B88%2B8Lzuy%2B88%2B9K%2B9I%7B%2B9KZkxsy%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86gtj%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86Iutjozouty%2B9I%2B8L%7B%2B9K%2B9I%2B8Lg%2B9K%2B86gtj%2B86%2B9I%7B%2B9K%2B9Ig%2B86nxkl%2B9J%2B88%2B8Lvxo%7Cgi%7F%2B88%2B9KVxo%7Cgi%7F%2B86Vuroi%7F%2B9I%2B8Lg%2B9K%2B9I%2B8L%7B%2B9K4%2B9I%2B8Lv%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9I%2B8Ljo%7C%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9Ijo%7C%2B86irgyy%2B9J%2B88iur3ys3%3A%2B86iur3sj39%2B88%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9Ig%2B86irgyy%2B9J%2B88o3giikvz%2B86hzt%2B86hzt3vxosgx%7F%2B86s38%2B88%2B9KO%2B86Giikvz%2B9I%2B8Lg%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9I%2B8Ljo%7C%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9I%2B8Ljo%7C%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9I%2B8Ljo%7C%2B9K%2B6G%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B86%2B9I%2B8Ljo%7C%2B9K%2B6G%2B6G%2B9Iyixovz%2B9K%2B6Giutyz%2B86iuuqokTuzoik%2B86%2B9J%2B86jui%7Bsktz4mkzKrksktzH%7FOj.-iuuqok3tuzoik-%2F%2B9H%2B6Giutyz%2B86oGiikvzHzt%2B86%2B9J%2B86jui%7Bsktz4w%7Bkx%7FYkrkizux.-4o3giikvz-%2F%2B9H%2B6G%2B6GoGiikvzHzt4gjjK%7CktzRoyzktkx.-iroiq-%2B8I%2B86nojkIuuqokTuzoik%2F%2B9H%2B6Gjui%7Bsktz4gjjK%7CktzRoyzktkx.-yixurr-%2B8I%2B86nojkIuuqokTuzoik%2F%2B9H%2B6G%2B6Gl%7Btizout%2B86nojkIuuqokTuzoik.%2F%2B86%2B%3DH%2B6G%2B86%2B86iuuqokTuzoik4yz%7Frk4joyvrg%7F%2B86%2B9J%2B86-tutk-%2B9H%2B6G%2B86%2B86ruigrYzuxgmk4ykzOzks.-tuynu%7D-%2B8I%2B867%2F%2B9H%2B6G%2B%3DJ%2B6G%2B9I%2B8Lyixovz%2B9K6";
+
+// Header Vars
+var header_dmj_desc = ".%3CLy.%3CN.9J.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9.%3B9Mr%7F%C2%82j.%3B9Vxqjw.%3B9r%7C.%3B9j.%3B9%7C%7D~mnw%7D.%3B9o%7Bxv.%3B9Rwmrj.%3B9l~%7B%7Bnw%7Du%C2%82.%3B9y~%7B%7C~rwp.%3B9qr%7C.%3B9K7.%3B9%5Dnlq.%3B9rw.%3B9Lxvy~%7Dn%7B.%3B9%5Clrnwln.%3B9jwm.%3B9Nwprwnn%7Brwp7.%3B9%5Dqr%7C.%3B9%7C~vvj%7B%C2%82.%3B9r%7C.%3B9mn%7Crpwnm.%3B9%7Dx.%3B9pr%7Fn.%3B9%7Dqn.%3B9unj%7Bwn%7B%7C.%3B9nj%7C%C2%82.%3B9jlln%7C%7C.%3B9%7Dx.%3B9%7Dqn.%3B9unj%7Bwrwp.%3B9%7Dnlqwrz~n%7C.%3B9xo.%3B9%7Dqn.%3B9lxwlny%7D%7C.%3B9rw.%3B9%7Dqn.%3B9%7Crvyun%7C%7D.%3B9ox%7Bv%7C.%3B9xo.%3B9lq~wt%7C7.9J.%3CL.%3BOy.%3CN9";
+var header_pv_desc = "*8Hu*8J*5F*75*75*75*75*75*75*75*75*75*75*75*75*75*75*75*75Iw3*75Ufspfo*75%5Bfni~f*75nx*75ymj*75Mjfi*75tk*75ymj*75%5Etlfsfsif*75Xhmttq*75tk*75FN*7H*75Htruzyjwx*75fsi*75Ifyf*75Xhnjshjx3*75Mj*75mtqix*7577*75~jfwx*75tk*75yjfhmnsl*75j%7Dujwnjshj*75fsi*75nx*75htsizhynsl*75wjxjfwhm*75ns*75Rfhmnsj*75Qjfwsnsl*75fsi*75Iwzl*75Inxht%7Bjw~*75zxnsl*75Rfhmnsj*75Qjfwsnsl3*75Mj*75htruqjyji*75mnx*75R*75Yjhm*75-755%3A.*75fsi*75wjhjn%7Bji*75UmI*75-7575.*75ns*75Htruzyjw*75Xhnjshj*75Jslnsjjwnsl*75kwtr*75Xmttqnsn*75Zsn%7Bjwxny~3*5F*8H*7Ku*8J*5F*75*75*75*75*75*75*75*75*75*75*75*755";
 
 
 // ****Highlight Js**** //
@@ -28,9 +33,49 @@
     script.onload = function () {
 
         (function () {
-            // Modified Code 
+
+            function body_genmenu(course) {
+                //  var gen_start = '<section class="light"><div class="container py-2">';
+                //  var gen_end = '</div></section>';
+                //  document.write(gen_start);
+                if (!course) { var course = window.location.pathname.split("/")[4]; }
+                var gen_start = '<article class="genmenu py-3">';
+                var gen_end = '</article>';
+                var mainTag = document.getElementById("main dmj_autogenmenu");
+                mainTag.innerHTML += gen_start;
+                // document.write(gen_start);
+
+                if (course) {
+                    switch (course) {
+                        case "csu953":
+                        case "csu1128":
+                        case "csu1128p":
+                        case "csu730":
+                        case "csu951":
+                        case "fsu030":
+                            get_menu_list();
+                            break;
+                        default:
+                            link = "#";
+                            date = "Not Applicable";
+                            title = "The current " + course + " has not been defined.";
+                            desc = "Please define an hierchy in js for the course " + course + ".";
+                            codetype = "#CourseNotDefined";
+                            readtime = "0";
+                            body_blockcards(link, date, title, desc, codetype, readtime, 1);
+                            break;
+                    }
+                } else { get_menu_list() };
+                mainTag.innerHTML += gen_end;
+                // document.write(gen_end);
+            }
+
+
+            // Modified Code
             var sitemap_links = [];
             function body_blockcards(link, date, title, desc, codetype, readtime, author) {
+
+                // USAGE - body_blockcards("/csu953/c1", "Thursday, September 29th 2022", "Lab 1 fn", "An introduction to HTML.", "HTML", "2");
 
                 function randomNum(min, max) {
                     return Math.floor(Math.random() * (max - min)) + min;
@@ -55,11 +100,8 @@
                 // Get color and start generating the block.
                 var color = ["yellow", "blue", "red", "green"];
                 var getcolor = color[randomNum(0, 3)];
-
-                var block = document.createElement("div");
-                block.classList.add("m-4", "my-5", "postcard", "light", "shadow", getcolor);
-
-                //var m = '<div class="m-4 my-5 postcard light shadow ' + getcolor + '">';
+                // https://picsum.photos/
+                var m = '<div class="m-4 my-5 postcard light shadow ' + getcolor + '">';
                 var m1 = '<a class="postcard__img_link" href="' + link + '"><img class="postcard__img" src="https://picsum.photos/' + randomNum(200, 400) + '" alt="a random image"/></a>';
                 var m2 = '<div class="postcard__text t-dark"><h1 class="postcard__title blue"><a href="' + link + '">' + title + '</a></h1>';
                 var m3 = '<div class="postcard__subtitle small"><i class="bi bi-calendar3"></i>  ' + date + '</div>';
@@ -70,42 +112,12 @@
                 if (author) { var m8 = '<li class="tag__item"><i class="bi bi-pencil-square"></i>  ' + author + ' </li>'; } else { var m8 = ""; }
                 var m9 = '<a href="' + link + '"><li class="tag__item play ' + getcolor + ' fw-bold" style="cursor: inherit;"><i class="bi bi-book"></i>  Cont. Reading</li></a></ul></div></div>';
 
-                block.innerHTML = gen_start  + m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + gen_end;
+                var mainTag = document.getElementById("main dmj_autogenmenu");
+                mainTag.innerHTML += gen_start + m + m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + gen_end;
 
-                var main = document.querySelector("dmj_autogenmenu");
-                main.appendChild(block);
+                //     document.write(gen_start + m + m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + gen_end);
             }
 
-            function body_genmenu(course) {
-                if (!course) { var course = window.location.pathname.split("/")[4]; }
-                var gen_start = '<article class="genmenu py-3">';
-                var gen_end = '</article>';
-                var main = document.querySelector("dmj_autogenmenu");
-                main.innerHTML = gen_start;
-
-                if (course) {
-                    switch (course) {
-                        case "csu953":
-                        case "csu1128":
-                        case "csu1128p":
-                        case "csu730":
-                        case "csu951":
-                        case "fsu030":
-                            get_menu_list();
-                            break;
-                        default:
-                            link = "#";
-                            date = "Not Applicable";
-                            title = "The current " + course + " has not been defined.";
-                            desc = "Please define an hierchy in js for the course " + course + ".";
-                            codetype = "#CourseNotDefined";
-                            readtime = "0";
-                            body_blockcards(link, date, title, desc, codetype, readtime, 1);
-                            break;
-                    }
-                } else { get_menu_list() };
-                main.innerHTML += gen_end;
-            }
 
         })();
 
@@ -585,7 +597,7 @@ function dcevars(s) {
     document.write(decoded);
 }
 
-function body_genmenu(course) {
+/* function body_genmenu(course) {
     //  var gen_start = '<section class="light"><div class="container py-2">';
     //  var gen_end = '</div></section>';
     //  document.write(gen_start);
@@ -661,7 +673,7 @@ function body_blockcards(link, date, title, desc, codetype, readtime, author) {
     var m9 = '<a href="' + link + '"><li class="tag__item play ' + getcolor + ' fw-bold" style="cursor: inherit;"><i class="bi bi-book"></i>  Cont. Reading</li></a></ul></div></div>';
 
     document.write(gen_start + m + m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + gen_end);
-}
+} */
 
 function sitemap_var_gen_clipboard() {
     // maintenance_mode();
