d388786779b634585f8b0c22be928117f4523f6e
Divya Mohan <divyamohan1993@users.noreply.github.com>
2023-02-03 22:58:08 +0530

    .

--- js/edu_su_common.js
+++ js/edu_su_common.js
@@ -988,13 +988,13 @@ function gen_blockquote() {
     document.head.appendChild(script1);
 
     var script2 = document.createElement('script');
-    script2.src = "https://www.googletagmanager.com/gtag/js?id=G-D8EG8249SV";
+    script2.src = "https://www.googletagmanager.com/gtag/js?id=G-GYE73EC1RV";
     document.head.appendChild(script2);
 
     script1.onload = function () { }
 
     script2.onload = function () {
-        window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-D8EG8249SV');
+        window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-GYE73EC1RV');
         (function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'dataLayer', 'GTM-PQ4VPSD');
         (function (c, l, a, r, i, t, y) { c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }; t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i; y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y); })(window, document, "clarity", "script", "fhsj5p7qsd");
     }
